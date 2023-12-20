fcdaefdea149e91a2c18d047e1141516145cf643
MayurTathe <mayurtathe02@gmail.com>
2023-12-20 20:38:24 +0530

    Added the Description on ProjectData.js file

--- src/components/Work/ProjectData.js
+++ src/components/Work/ProjectData.js
@@ -2,7 +2,7 @@ export const projects = [
   {
     id: 1,
     title: 'YouTube Clone',
-    description:'Build and deployed YouTube_Clone project. Used Tailwind CSS & Material UI icons. New Technology Learned: Tailwind CSS',
+    description:'Build and deployed YouTube_Clone project. Used Tailwind CSS & Material UI icons. New Technology Learned: Tailwind CSS.',
     image: '../YouTube.png', // Replace with actual image URL
     color: '#618264',
     projectUrl: 'https://github.com/MayurTathe/YouTube_Clone'
@@ -10,7 +10,7 @@ export const projects = [
   {
     id: 2,
     title: 'Portfolio',
-    description: 'Self designed and developed a responsive Portfolio form Scratch. Used Farmer Motion and Tailwind CSS. New Technology Learned: Farmer Motion',
+    description: 'Self designed and developed a responsive Portfolio form Scratch. Used Farmer Motion and Tailwind CSS. New Technology Learned: Farmer Motion.',
     image: 'EMS-R.png', // Replace with actual image URL
     color: '#164863',
     projectUrl: ""
@@ -18,7 +18,7 @@ export const projects = [
   {
     id: 3,
     title: 'Employee Management System (EMS)',
-    description: 'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    description: 'Developed a Full-Stack CRUD operation Project using React-JS. Used Spring Boot, MySQL, Axios(for API Integration). New Technology Learned: React & Axios.',
     image: 'EMS-R.png', // Replace with actual image URL
     color: '#016A70',
     projectUrl: 'https://github.com/MayurTathe/ReactCrud_Frontend'
@@ -26,7 +26,7 @@ export const projects = [
   {
     id: 4,
     title: 'Angular CRUD',
-    description: 'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    description: 'Developed a Full-Stack CRUD operation Project using Angular (as Frontend). Used Spring Boot (as Backend), MySQL (Database).',
     image: 'EMS-R.png', // Replace with actual image URL
     color: '#D0E7D2',
     projectUrl: 'https://github.com/MayurTathe/AngularCrud_Frontend'
